cca13a90d1df7a7165d0dac245d1ec701e560f12
surendra <surendravikram85@gmail.com>
2020-12-15 22:59:26 +0530

    initial commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,29 @@
+import { MyInterceptor } from './jwt.interceptor';
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http';
+import { CommonModule } from "@angular/common";
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import {HTTP_INTERCEPTORS} from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
+
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    HttpClientModule,
+    CommonModule,
+    FormsModule
+  ],
+  providers: [{
+    provide: HTTP_INTERCEPTORS,
+    useClass: MyInterceptor,
+    multi: true,
+  }],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
